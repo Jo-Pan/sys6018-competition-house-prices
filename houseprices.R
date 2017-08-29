aa28f1b1b9237506b234998fa9eec68b3d806519
Jo Pan <pan.huitong@yahoo.com>
2017-08-29 19:05:45 -0400

    cleaned na & built a basic lm

--- houseprices.R
+++ houseprices.R
@@ -1,4 +1,4 @@
-#setwd("/Users/Pan/Google Drive/Data Science/SYS 6018/sys6018-competition-house-prices")
+setwd("/Users/Pan/Google Drive/Data Science/SYS 6018/sys6018-competition-house-prices")
 library(readr)
 library(dplyr)
 
@@ -20,9 +20,40 @@ na_list<-colSums(is.na(train)) #count na in each col
 na_list[na_list!=0]            #print those cols with na
 names(na_list[na_list!=0])
 
+#fix numeric_var's na
+na_list[numeric_var]
+train$GarageYrBlt[is.na(train$GarageYrBlt)] <- 0
+train$MasVnrArea[is.na(train$MasVnrArea)] <- 0
+train$LotFrontage[is.na(train$LotFrontage)] <- 0
+
+#fix factor_var's na based on data_description 
+train[,fac_var]<-lapply(train[,fac_var],as.character) #convert factor variables to character
+                                                  
+
+train$Alley[is.na(train$Alley)]<-'na'  #No alley access
+train$BsmtQual[is.na(train$BsmtQual)]<-'na' #No Basement
+train$BsmtCond[is.na(train$BsmtCond)]<-'na' #No Basement
+train$BsmtExposure[is.na(train$BsmtExposure)]<-'na'#No Basement
+train$BsmtFinType1[is.na(train$BsmtFinType1)]<-'na'#No Basement
+train$BsmtFinType2[is.na(train$BsmtFinType2)]<-'na'#No Basement
+train$FireplaceQu[is.na(train$FireplaceQu)]<-'na'#No Fireplace
+train$GarageType[is.na(train$GarageType)]<-'na'#No Garage
+train$GarageQual[is.na(train$GarageQual)]<-'na'#No Garage
+train$GarageCond[is.na(train$GarageCond)]<-'na'#No Garage
+train$GarageFinish[is.na(train$GarageFinish)]<-'na'#No Garage
+train$PoolQC[is.na(train$PoolQC)]<-'na'#No Pool
+train$Fence[is.na(train$Fence)]<-'na'#No Fence
+train$MiscFeature[is.na(train$MiscFeature)]<-'na'#None
+train$MasVnrType[is.na(train$MasVnrType)]<-'None'  #there is a none category, so just make na to none
+train$Electrical[is.na(train$Electrical)]<-'na'  #there is only 1 na
+
+train[,fac_var]<-lapply(train[,fac_var],factor) #convert character variables to factors
+colSums(is.na(train)) #no more na!
+
 # --------------- Parametric ----------------
 lm1 <- lm(SalePrice ~., data=train)
-summary(lm1)
+summary(lm1) #Adjusted R-squared:  0.9192 
+
 # --------------- Non-Parametric ------------
 
 
