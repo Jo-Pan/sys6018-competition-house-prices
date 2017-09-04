01540f6dfeaa7bc400cdf846badee4beb69d411a
isabellexliu <xl9qw@virginia.edu>
2017-09-04 13:15:41 -0400

    parametric cross validation done

--- Competition 2.R
+++ Competition 2.R
@@ -95,62 +95,106 @@ comb[,fac_var]<-lapply(comb[,fac_var],factor) #convert character variables to fa
 comb_dum <- dummy.data.frame(comb, sep = ".")
 
 # --------------- Separate Train for Cross validation ----------
-mytrain=comb_dum[comb_dum$dataset=='train',c(1:80,82)]
+alltrain=comb_dum[comb_dum$dataset.train==1,c(1:303,306)]
+alltest=comb_dum[comb_dum$dataset.test==1,c(1:303,306)]
 mytrain.index=c(sample(1:1460,730))
-mytrain.1<-mytrain[mytrain.index,]
-mytest.1<-mytrain[-mytrain.index,]
+mytrain<-alltrain[mytrain.index,]
+mytest<-alltrain[-mytrain.index,]
 
 # --------------- Parametric ----------------
 # Create the original regression model using all variables
-lm1 <- lm(SalePrice ~., data=mytrain.1)
-summary(lm1) # Adjusted R-squared:  0.9337
+lm1 <- lm(SalePrice ~., data=mytrain)
+summary(lm1) # Adjusted R-squared:  0.9363 
 
 # Test the model on the validation set
-test.lm1 <- lm(SalePrice ~.,data=mytest.1)
-summary(test.lm1) # Adjusted R-squared:  0.9186
+test.lm1 <- lm(SalePrice ~.,data=mytest)
+summary(test.lm1) # Adjusted R-squared:  0.9323 
 mse1 <- mean(test.lm1$residuals^2)
-mse1 # 332500730
+mse1 # 244012820
 
 # Trying to find the best fit by running all possible combination of variables
 # compare the significance level of each variable and the value for adjusted R-squared
 library(MASS)
-fit <- lm(SalePrice ~., data=mytrain.1)
+fit <- lm(SalePrice ~., data=mytrain)
 step <- stepAIC(fit, direction="both")
 step$anova
 
 # Determine another regression model based on stepwise fit
-lm2 <- lm(SalePrice ~ Id + MSSubClass + MSZoning + LotArea + Street + LotShape + 
-            LandContour + Utilities + LotConfig + Neighborhood + Condition1 + 
-            Condition2 + HouseStyle + OverallQual + OverallCond + YearBuilt + 
-            RoofMatl + MasVnrType + MasVnrArea + ExterQual + ExterCond + 
-            BsmtQual + BsmtExposure + BsmtFinType1 + BsmtFinSF1 + BsmtFinSF2 + 
-            BsmtUnfSF + X1stFlrSF + X2ndFlrSF + LowQualFinSF + FullBath + 
-            BedroomAbvGr + KitchenAbvGr + KitchenQual + TotRmsAbvGrd + 
-            Functional + FireplaceQu + GarageCars + GarageArea + GarageCond + 
-            WoodDeckSF + OpenPorchSF + EnclosedPorch + ScreenPorch + 
-            PoolQC + SaleCondition, data=mytrain.1)
-summary(lm2) # Adjusted R-squared:  0.9312
+lm2 <- lm(SalePrice ~ `MSZoning.C (all)` + MSZoning.FV + LotFrontage + 
+            LotArea + Street.Grvl + LotShape.IR2 + LandContour.Low + 
+            Utilities.AllPub + LotConfig.CulDSac + LandSlope.Gtl + LandSlope.Mod + 
+            Neighborhood.Blueste + Neighborhood.CollgCr + Neighborhood.Crawfor + 
+            Neighborhood.Edwards + Neighborhood.Gilbert + Neighborhood.IDOTRR + 
+            Neighborhood.Mitchel + Neighborhood.NAmes + Neighborhood.NoRidge + 
+            Neighborhood.NridgHt + Neighborhood.NWAmes + Neighborhood.OldTown + 
+            Neighborhood.StoneBr + Condition1.Artery + Condition1.Feedr + 
+            Condition1.RRAe + Condition2.Feedr + Condition2.PosN + Condition2.RRAe + 
+            BldgType.1Fam + BldgType.2fmCon + BldgType.Twnhs + HouseStyle.1.5Fin + 
+            HouseStyle.2.5Fin + HouseStyle.2.5Unf + HouseStyle.2Story + 
+            OverallQual + OverallCond + YearBuilt + YearRemodAdd + RoofStyle.Flat + 
+            RoofStyle.Gable + RoofStyle.Gambrel + RoofStyle.Hip + RoofStyle.Mansard + 
+            RoofMatl.Metal + Exterior1st.BrkComm + Exterior1st.BrkFace + 
+            Exterior1st.CemntBd + Exterior1st.HdBoard + Exterior2nd.AsphShn + 
+            Exterior2nd.CmentBd + Exterior2nd.Plywood + MasVnrType.BrkCmn + 
+            MasVnrType.BrkFace + MasVnrArea + ExterQual.Ex + ExterCond.Gd + 
+            Foundation.Slab + BsmtQual.Gd + BsmtQual.na + BsmtCond.Fa + 
+            BsmtCond.Gd + BsmtCond.Po + BsmtExposure.Gd + BsmtFinType1.ALQ + 
+            BsmtFinType1.BLQ + BsmtFinType1.LwQ + BsmtFinType1.Rec + 
+            BsmtFinSF1 + BsmtFinType2.ALQ + BsmtFinType2.LwQ + BsmtFinSF2 + 
+            BsmtUnfSF + HeatingQC.Po + Electrical.FuseP + X1stFlrSF + 
+            X2ndFlrSF + BedroomAbvGr + KitchenQual.Ex + Functional.Maj1 + 
+            Functional.Maj2 + Functional.Min1 + Functional.Mod + Fireplaces + 
+            FireplaceQu.na + GarageType.Attchd + GarageType.Basment + 
+            GarageType.BuiltIn + GarageType.CarPort + GarageType.Detchd + 
+            GarageYrBlt + GarageFinish.RFn + GarageCars + GarageQual.Fa + 
+            WoodDeckSF + OpenPorchSF + ScreenPorch + PoolArea + PoolQC.Ex + 
+            Fence.GdPrv + MiscFeature.na + MoSold + SaleType.CWD + SaleCondition.Abnorml + 
+            SaleCondition.Alloca + SaleCondition.Family + SaleCondition.Normal + 
+            ExterQual.TA, data=mytrain)
+summary(lm2) # Adjusted R-squared:  0.8827
 
 # Test the model on the validation set
-test.lm2 <- lm(SalePrice ~ Id + MSSubClass + MSZoning + LotArea + Street + LotShape + 
-                 LandContour + Utilities + LotConfig + Neighborhood + Condition1 + 
-                 Condition2 + HouseStyle + OverallQual + OverallCond + YearBuilt + 
-                 RoofMatl + MasVnrType + MasVnrArea + ExterQual + ExterCond + 
-                 BsmtQual + BsmtExposure + BsmtFinType1 + BsmtFinSF1 + BsmtFinSF2 + 
-                 BsmtUnfSF + X1stFlrSF + X2ndFlrSF + LowQualFinSF + FullBath + 
-                 BedroomAbvGr + KitchenAbvGr + KitchenQual + TotRmsAbvGrd + 
-                 Functional + FireplaceQu + GarageCars + GarageArea + GarageCond + 
-                 WoodDeckSF + OpenPorchSF + EnclosedPorch + ScreenPorch + 
-                 PoolQC + SaleCondition, data=mytest.1)
-summary(test.lm2) # Adjusted R-squared:  0.9373
+test.lm2 <- lm(SalePrice ~ `MSZoning.C (all)` + MSZoning.FV + LotFrontage + 
+                 LotArea + Street.Grvl + LotShape.IR2 + LandContour.Low + 
+                 Utilities.AllPub + LotConfig.CulDSac + LandSlope.Gtl + LandSlope.Mod + 
+                 Neighborhood.Blueste + Neighborhood.CollgCr + Neighborhood.Crawfor + 
+                 Neighborhood.Edwards + Neighborhood.Gilbert + Neighborhood.IDOTRR + 
+                 Neighborhood.Mitchel + Neighborhood.NAmes + Neighborhood.NoRidge + 
+                 Neighborhood.NridgHt + Neighborhood.NWAmes + Neighborhood.OldTown + 
+                 Neighborhood.StoneBr + Condition1.Artery + Condition1.Feedr + 
+                 Condition1.RRAe + Condition2.Feedr + Condition2.PosN + Condition2.RRAe + 
+                 BldgType.1Fam + BldgType.2fmCon + BldgType.Twnhs + HouseStyle.1.5Fin + 
+                 HouseStyle.2.5Fin + HouseStyle.2.5Unf + HouseStyle.2Story + 
+                 OverallQual + OverallCond + YearBuilt + YearRemodAdd + RoofStyle.Flat + 
+                 RoofStyle.Gable + RoofStyle.Gambrel + RoofStyle.Hip + RoofStyle.Mansard + 
+                 RoofMatl.Metal + Exterior1st.BrkComm + Exterior1st.BrkFace + 
+                 Exterior1st.CemntBd + Exterior1st.HdBoard + Exterior2nd.AsphShn + 
+                 Exterior2nd.CmentBd + Exterior2nd.Plywood + MasVnrType.BrkCmn + 
+                 MasVnrType.BrkFace + MasVnrArea + ExterQual.Ex + ExterCond.Gd + 
+                 Foundation.Slab + BsmtQual.Gd + BsmtQual.na + BsmtCond.Fa + 
+                 BsmtCond.Gd + BsmtCond.Po + BsmtExposure.Gd + BsmtFinType1.ALQ + 
+                 BsmtFinType1.BLQ + BsmtFinType1.LwQ + BsmtFinType1.Rec + 
+                 BsmtFinSF1 + BsmtFinType2.ALQ + BsmtFinType2.LwQ + BsmtFinSF2 + 
+                 BsmtUnfSF + HeatingQC.Po + Electrical.FuseP + X1stFlrSF + 
+                 X2ndFlrSF + BedroomAbvGr + KitchenQual.Ex + Functional.Maj1 + 
+                 Functional.Maj2 + Functional.Min1 + Functional.Mod + Fireplaces + 
+                 FireplaceQu.na + GarageType.Attchd + GarageType.Basment + 
+                 GarageType.BuiltIn + GarageType.CarPort + GarageType.Detchd + 
+                 GarageYrBlt + GarageFinish.RFn + GarageCars + GarageQual.Fa + 
+                 WoodDeckSF + OpenPorchSF + ScreenPorch + PoolArea + PoolQC.Ex + 
+                 Fence.GdPrv + MiscFeature.na + MoSold + SaleType.CWD + SaleCondition.Abnorml + 
+                 SaleCondition.Alloca + SaleCondition.Family + SaleCondition.Normal + 
+                 ExterQual.TA, data=mytest)
+summary(test.lm2) # Adjusted R-squared:  0.9306
 mse2 <- mean(test.lm2$residuals^2)
-mse2 # 375873720
+mse2 # 313409832
 
 # Using the better model (larger R-squared value and smaller mse) to predict
-predictions <- as.vector(predict(lm2, newdata=comb_dum[comb_dum$dataset=='test',1:80]))
+lm1 <- lm(SalePrice ~., data=alltrain)
+predictions <- as.vector(predict(lm1, newdata=alltest))
 table <- data.frame(test$Id, predictions)
 colSums(is.na(table))  #no weird predictions
-write.table(table, file="housepricesprediction1.csv", row.names=F, col.names=c("Id", "SalePrice"), sep=",")
+write.table(table, file="housepricesprediction.csv", row.names=F, col.names=c("Id", "SalePrice"), sep=",")
 
 # --------------- Non-Parametric ------------
 
